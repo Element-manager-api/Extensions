a10e52b87e4c5360b465daebfa0746c7f58ade49
Element-manager-api <80879988+Element-manager-api@users.noreply.github.com>
2023-02-27 16:44:50 +0530

    Add files via upload

---
+++ ReportApp/scripts/js/initalizeConsoleAddin.js
@@ -0,0 +1,26 @@
+var initalizeConsoleAddin = (function () {
+    function initalizeConsoleAddin() {
+    }
+    initalizeConsoleAddin.prototype.initialize = function () {
+        var id = "reportApp";
+        var label = "Report App";
+        var isDisable = false;
+        ORACLE_SERVICE_CLOUD.extension_loader.load("ConsoleApp").then(function (extensionProvider) {
+            extensionProvider.registerUserInterfaceExtension(function (userInterfaceExtension) {
+                userInterfaceExtension.getLeftSidePaneContext().then(function (leftSidePaneContext) {
+                    leftSidePaneContext.getSidePane(id).then(function (leftPanelContext) {
+                        var icon = leftPanelContext.createIcon('font awesome');
+                        icon.setIconClass("fa fa-line-chart");
+                        icon.setIconColor("red");
+                        leftPanelContext.addIcon(icon);
+                        leftPanelContext.setContentUrl("../pages/reportApp.html");
+                        leftPanelContext.setLabel(label);
+                        leftPanelContext.setDisabled(isDisable);
+                        leftPanelContext.render();
+                    });
+                });
+            });
+        });
+    };
+    return initalizeConsoleAddin;
+}());
